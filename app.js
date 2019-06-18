ce33c0e79b2271cf5d6461d3de009a9521b2b561
park <ckdwls2525@naver.com>
2019-06-18 19:01:17 +0900

    express route

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const indexRouter = require('./route/index');
+const userRouter = require('./route/users');
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
 
 const PORT = process.env.PORT || 5000;
 
