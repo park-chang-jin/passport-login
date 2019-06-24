6c9f233cbfc489befa2a5a65455983998fd0acba
park <ckdwls2525@naver.com>
2019-06-24 18:31:31 +0900

    logout...finish

--- route/index.js
+++ route/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/', (req, res) => {
     res.render('welcome');
 });
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+router.get('/dashboard', ensureAuthenticated, (req, res, next) => {
+    res.render('dashboard', {
+         name: req.user.name
+    });
 });
 
 module.exports = router;
